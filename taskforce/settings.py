fd9581ab1acf4bfa3270db37c1b370f78a2b605f
david <daviddaudadeji@gmail.com>
2023-12-23 13:00:58 +0000

    fuck it

--- taskforce/settings.py
+++ taskforce/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 MOLLIE_API_KEY = 'test_9cpV9y2UtfS2KR4SDeUVp4Vkp2tBWg'
